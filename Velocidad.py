5370fa9844094e9e5f5ec3f058de2c8b56a541c0
YasminLN12 <46935028+YasminLN12@users.noreply.github.com>
2019-01-22 11:04:39 -0600

    Yasmín Landaverde Nava, Grupo 03

---
+++ Velocidad.py
@@ -0,0 +1,9 @@
+#Autor: Yasmín Landaverde
+#Calcula la verlocidad promedio, dado el tiempo y la distancia
+
+distancia = int(input("teclea la distancia de tu viaje en kms enteros: "))
+tiempo = int(input("teclea el tiempo de tu viaje en hrs enteras: "))
+
+velocidad = distancia/tiempo
+
+print("velocidad promedio: ", velocidad, "km/h")
